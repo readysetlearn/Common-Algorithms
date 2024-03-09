7941ecc8993a7e1199728e0062417324d309f932
Josh <josh@exmaple.com>
2024-03-08 22:36:41 -0800

    Added 2 more tests

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -1,7 +1,8 @@
 // Implementation of binary search https://en.wikipedia.org/wiki/Binary_search_algorithm
 #include <iostream>
 #include <vector>
-#include <cassert>
+#include <set>
+#include <list>
 
 // Return iterator pointing to the location where target was found. If target was not found, the location of where it would be is returned.
 template <typename ForwardIt, typename T>
@@ -87,6 +88,56 @@ void test_value_not_present()
 	{
 		std::cout << "Test 5 (Value not present): Failed" << std::endl;
 	}
+}
+
+// Test 6: Vector of doubles
+void test_vector_double()
+{
+	std::vector<double> vec = {-2.1, 1.1, 3.5, 3.7, 4.1};
+	const auto target = 1.1;
+	auto result = binary_search_position(vec.begin(), vec.end(), target);
+	if(*result == target)
+	{
+		std::cout << "Test 6 (vector of doubles): Passed" << std::endl;
+	}
+	else
+	{
+		std::cout << "Test 6 (vector of doubles): Failed. Expected " << target << ", Actual: " << *result << std::endl;
+	}
+}
+
+// Test 7: Set of strings
+void test_set_string()
+{
+	std::set<std::string> testSet = {"apples", "oranges", "bananas", "onions"};
+	const auto target = "onions";
+	auto result = binary_search_position(testSet.begin(), testSet.end(), target);
+	if(*result == target)
+	{
+		std::cout << "Test 7 (set of strings): Passed" << std::endl;
+	}
+	else
+	{
+		std::cout << "Test 7 (set of strings): Failed. Expected " << target << ", Actual: " << *result << std::endl;
+	}
+	
+}
+
+// Test 8: List of ints
+void test_list_int()
+{
+	std::list<int> testList = {-10, 10, 20, 20, 30, 30, 40, 50};
+	const auto target = 30;
+	auto result = binary_search_position(testList.begin(), testList.end(), target);
+	if(*result == target)
+	{
+		std::cout << "Test 8 (list of ints): Passed" << std::endl;
+	}
+	else
+	{
+		std::cout << "Test 8 (list of ints): Failed. Expected " << target << ", Actual: " << *result << std::endl;
+	}
+	
 }
 
 int main()
