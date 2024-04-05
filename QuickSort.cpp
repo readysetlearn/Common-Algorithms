ac157b8249e0b7dacc12ff003f6e50460ef7b414
Josh <josh@exmaple.com>
2024-04-05 14:23:29 -0700

    Implemented slightly different algorithm to reduce recursion depth. Now the smaller partition is recursed, and the larger one is iterated through. This avoids stack overflows on some inputs where the recursion depth gets to large. While this change may not improve execution time on most inputs, in situations where there are lots of duplicates it seems to help.

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -3,11 +3,13 @@
 #include <vector>
 #include <list>
 #include <cassert>
-#include <algorithm> // for std::is_sorted
 
 // Function prototypes
 template<typename Iter>
 void quickSort(Iter begin, Iter end);
+
+template<typename Iter>
+void qs(Iter begin, Iter end);
 
 template <typename Iter>
 Iter Partition(Iter begin, Iter end);
@@ -21,27 +23,39 @@ void sort2(Iter begin, Iter end);
 template <typename Iter>
 void sort3(Iter begin, Iter end);
 
-// Sorts a range of elements using the Quick Sort algorithm.
+
+//Wrapper function to account for std::end returning past the end iterator
 template<typename Iter>
 void quickSort(Iter begin, Iter end)
 {
-    if (std::distance(begin, end) < 4)
-	{
-		if (std::distance(begin, end) == 3){ sort3(begin, end);} // More efficient to manually sort 2 or 3 elements than recurse
-		else if (std::distance(begin, end) == 2){ sort2(begin, end);}
-		return; // Partitions of size less than 2 are sorted
-	}
+	if(begin == end) return; // Empty vector
+	qs(begin, std::prev(end));
+}
 
-    Iter pi = Partition(begin, end); // pi is partition index
-	if(std::distance(begin, pi) < std::distance(std::next(pi), end)) // recurse on smaller partition first for efficiency
+// Sorts a range of elements using the Quick Sort algorithm.
+template<typename Iter>
+void qs(Iter begin, Iter end)
+{
+	while(std::distance(begin, end) > 0)
 	{
-		quickSort(begin, pi);
-		quickSort(std::next(pi), end);
-	}
-	else
-	{
-		quickSort(std::next(pi), end);
-		quickSort(begin, pi);
+		//std::cout << "distance from begin to end: " << std::distance(begin, end) << std::endl;
+		if (std::distance(begin, end) < 3)
+		{
+			if (std::distance(begin, end) == 2){ sort3(begin, end);} // More efficient to manually sort 2 or 3 elements than recurse
+			else if (std::distance(begin, end) == 1){ sort2(begin, end);}
+			return; // Partitions of size less than 2 are sorted
+		}
+		Iter pi = Partition(begin, end); // pi is partition index
+		if(std::distance(begin, pi) > std::distance(pi, end)) // recurse smaller partition first
+		{
+			qs(std::next(pi), end);
+			end = std::prev(pi);
+		}
+		else
+		{
+			qs(begin, pi);
+			begin = std::next(pi);
+		}
 	}
 }
 
@@ -49,11 +63,11 @@ void quickSort(Iter begin, Iter end)
 template <typename Iter>
 Iter Partition(Iter begin, Iter end)
 {
-    Iter lft = begin; // Initialize left index
-    Iter rgt = std::prev(end); // Initialize right index
+	Iter lft = begin; // Initialize left index
+	Iter rgt = end; // Initialize right index
 	auto pivot = *medianOf3(lft, rgt);
 
-    while(true) 
+	while(true) 
 	{
 		while (*lft < pivot)
 		{
@@ -67,8 +81,8 @@ Iter Partition(Iter begin, Iter end)
 
 		if(std::distance(lft, rgt) <= 0)
 		{
-            return rgt;
-        }
+			return rgt;
+		}
 		
 		// Check if left and right point to equal elements
 		// This check isn't needed if input has no duplicates
@@ -77,10 +91,10 @@ Iter Partition(Iter begin, Iter end)
 			std::advance(lft, 1);
 		}
 		else
-        {
+		{
 			std::iter_swap(lft, rgt);
 		}
-    }
+	}
 }
 
 // @return Median value among the first, middle and last element
@@ -109,7 +123,7 @@ Iter medianOf3(Iter begin, Iter end)
 template <typename Iter>
 void sort2(Iter begin, Iter end)
 {
-	std::advance(end, -1);
+	//std::advance(end, -1);
 	if(*end < *begin)
 	{
 		std::iter_swap(begin, end);
@@ -121,7 +135,7 @@ void sort2(Iter begin, Iter end)
 template <typename Iter>
 void sort3(Iter begin, Iter end)
 {
-	std::advance(end, -1);
+	//std::advance(end, -1);
 	auto mid = std::next(begin);
 	assert(std::next(mid) == end);
 	if(*begin > *end)
@@ -143,186 +157,171 @@ void sort3(Iter begin, Iter end)
 // Test case 1: empty vector
 void testEmptyRange()
 {
-    std::vector<int> vec;  // Empty vector
-    quickSort(vec.begin(), vec.end());
-    assert(vec.size() == 0);
+	std::vector<int> vec;  // Empty vector
+	quickSort(vec.begin(), (vec.end()));
+	assert(vec.size() == 0);
 }
 
 // Test case 2: vector with only one element.
 void testSingleElementRange()
 {
-    std::vector<int> vec = {42};  // Single-element vector
-    quickSort(vec.begin(), vec.end());
-    assert(vec.size() == 1);
-    assert(vec[0] == 42);
+	std::vector<int> vec = {42};  // Single-element vector
+	quickSort(vec.begin(), vec.end());
+	assert(vec.size() == 1);
+	assert(vec[0] == 42);
 }
 
 // Test case 3: vector where elements are already sorted
 void testSortedRange()
 {
-    std::vector<int> vec = {1, 2, 3, 4, 5};  // Sorted vector
-    quickSort(vec.begin(), vec.end());
-    assert((vec == std::vector<int>{1, 2, 3, 4, 5}));
+	std::vector<int> vec = {1, 2, 3, 4, 5};  // Sorted vector
+	quickSort(vec.begin(), vec.end());
+	assert((vec == std::vector<int>{1, 2, 3, 4, 5}));
 }
 
 // Test case 4: vector where elements are sorted in descending order.
 void testReverseSortedRange()
 {
-    std::vector<int> vec = {5, 4, 3, 2, 1};  // Reverse-sorted vector
-    quickSort(vec.begin(), vec.end());
-    assert((vec == std::vector<int>{1, 2, 3, 4, 5}));
+	std::vector<int> vec = {5, 4, 3, 2, 1};  // Reverse-sorted vector
+	quickSort(vec.begin(), vec.end());
+	assert((vec == std::vector<int>{1, 2, 3, 4, 5}));
 }
 
 // Test case 5: vector with random ints
 void testRandomdRangeInt()
 {
-    std::vector<int> vec = {3, 1, 4, -2, 5};  // Random vector
-    quickSort(vec.begin(), vec.end());
-    assert((vec == std::vector<int>{-2, 1, 3, 4, 5}));
+	std::vector<int> vec = {3, 1, 4, -2, 5};  // Random vector
+	quickSort(vec.begin(), vec.end());
+	assert((vec == std::vector<int>{-2, 1, 3, 4, 5}));
 }
 
 // Test case 6: vector with random chars
 void testRandomRangeChar()
 {
-    std::vector<char> vec = {'c', 'b', 'a', 'z', 'x', 'y'};  // Random vector
-    quickSort(vec.begin(), vec.end());
-    assert((vec == std::vector<char>{'a', 'b', 'c', 'x', 'y', 'z'}));
+	std::vector<char> vec = {'c', 'b', 'a', 'z', 'x', 'y'};  // Random vector
+	quickSort(vec.begin(), vec.end());
+	assert((vec == std::vector<char>{'a', 'b', 'c', 'x', 'y', 'z'}));
 }
 
 // Test case 7: vector with random ints and duplicates
 void testDuplicateElements()
 {
-    std::vector<int> vec = {3, 1, 4, 2, 1, 5, 4};  // Vector with duplicates
-    quickSort(vec.begin(), vec.end());
-    assert((vec == std::vector<int>{1, 1, 2, 3, 4, 4, 5}));
+	std::vector<int> vec = {3, 1, 4, 2, 1, 5, 4};  // Vector with duplicates
+	quickSort(vec.begin(), vec.end());
+	assert((vec == std::vector<int>{1, 1, 2, 3, 4, 4, 5}));
 }
 
 // Test case 8: vector with all duplicates, odd length
 void allDupsOdd()
 {
-    std::vector<int> vec(5, 10);  // Odd length vector of duplicates
-    quickSort(vec.begin(), vec.end());
-    assert(vec == std::vector<int>(5, 10));
+	std::vector<int> vec(5, 10);  // Odd length vector of duplicates
+	quickSort(vec.begin(), vec.end());
+	assert(vec == std::vector<int>(5, 10));
 }
 
 // Test case 9: vector with all duplicates, even length
 void allDupsEven()
 {
-    std::vector<int> vec(6, 10);  // Even length vector of duplicates
-    quickSort(vec.begin(), vec.end());
-    assert(vec == std::vector<int>(6, 10));
+	std::vector<int> vec(6, 10);  // Even length vector of duplicates
+	quickSort(vec.begin(), vec.end());
+	assert(vec == std::vector<int>(6, 10));
 }
 
 // Test case 10: list with random ints
 void testRandomListSort()
 {
-    std::list<int> randomList = {30, 10, 50, 20, 40}; // Random list
-    quickSort(randomList.begin(), randomList.end());
-    assert((randomList == std::list<int>{10, 20, 30, 40, 50}));
+	std::list<int> randomList = {30, 10, 50, 20, 40}; // Random list
+	quickSort(randomList.begin(), randomList.end());
+	assert((randomList == std::list<int>{10, 20, 30, 40, 50}));
 }
 
 // Test case 11: large vector with random longs
 void testLongRand()
 {
-    std::vector<long> vec;
-    for(long i = -100; i <= 500000; i++)
-    {
-      if(i % 5 == 0) vec.push_back(i -20);
-      else if(i % 3 == 0) vec.push_back(i + 20);
-      else if(i % 7 == 0) vec.push_back(i + 1);
-      else vec.push_back(i);
-    }
-    
-    quickSort(vec.begin(), vec.end());
-    
-    for(size_t i = 1; i < vec.size(); i++)
-    {
-      if(vec.at(i) < vec.at(i-1))
-      {
-        std::cout << "sort failed at values " << vec.at(i-1) << " and " << vec.at(i) << std::endl;
-        break;
-      }
-    }
+	std::vector<long> vec;
+	for(long i = -10000; i <= 50000000; i++)
+	{
+	  if(i % 5 == 0) vec.push_back(i - 20);
+	  else if(i % 3 == 0) vec.push_back(i + 20);
+	  else if(i % 7 == 0) vec.push_back(i + 1);
+	  else vec.push_back(i);
+	}
+	
+	quickSort(vec.begin(), vec.end());
+	
+	for(size_t i = 1; i < vec.size(); i++)
+	{
+	  if(vec.at(i) < vec.at(i-1))
+	  {
+		std::cout << "sort failed at values " << vec.at(i-1) << " and " << vec.at(i) << std::endl;
+		break;
+	  }
+	}
 }
 
 // Test case 12: large vector of duplicates
 void testLongDups()
 {
-    std::vector<long> vec;
-    for(long i = 1; i <= 20000; i++)
-    {
-      vec.push_back(321);
-    }
-    
-    const auto vec2 = vec;
-    
-    quickSort(vec.begin(), vec.end());
-    
-    assert(vec == vec2);
+	std::vector<long> vec;
+	for(long i = 1; i <= 100000; i++)
+	{
+	  vec.push_back(321);
+	}
+	
+	const auto vec2 = vec;
+	
+	quickSort(vec.begin(), vec.end());
+	
+	assert(vec == vec2);
 }
 
 
 // medianOf3 tests
 // Test case 1: Three distinct elements
 void test3Distinct() {
-    std::vector<int> nums = {5, 2, 8};
-    auto result = medianOf3(nums.begin(), std::prev(nums.end()));
-    assert(*result == 5);
+	std::vector<int> nums = {5, 2, 8};
+	auto result = medianOf3(nums.begin(), std::prev(nums.end()));
+	assert(*result == 5);
 }
 
 // Test case 2: Three equal elements
 void test3Equal() {
-    std::vector<int> nums = {4, 4, 4};
-    auto result = medianOf3(nums.begin(), std::prev(nums.end()));
-    assert(*result == 4);
+	std::vector<int> nums = {4, 4, 4};
+	auto result = medianOf3(nums.begin(), std::prev(nums.end()));
+	assert(*result == 4);
 }
 
 // Test case 3: Ascending elements
 void test3Ascending() {
-    std::vector<int> nums = {1, 2, 3};
-    auto result = medianOf3(nums.begin(), std::prev(nums.end()));
-    assert(*result == 2);
+	std::vector<int> nums = {1, 2, 3};
+	auto result = medianOf3(nums.begin(), std::prev(nums.end()));
+	assert(*result == 2);
 }
 
 // Test case 4: Descending elements
 void test3Descending() {
-    std::vector<int> nums = {6, 5, 4};
-    auto result = medianOf3(nums.begin(), std::prev(nums.end()));
-    assert(*result == 5);
+	std::vector<int> nums = {6, 5, 4};
+	auto result = medianOf3(nums.begin(), std::prev(nums.end()));
+	assert(*result == 5);
 }
 
 // Test case 5: Six chars
 void test6Chars() {
-    std::vector<int> chars = {'x', 'y', 'z', 'a', 'b', 'c'};
-    auto result = medianOf3(chars.begin(), std::prev(chars.end()));
-    assert(*result == 'x');
+	std::vector<int> chars = {'x', 'y', 'z', 'a', 'b', 'c'};
+	auto result = medianOf3(chars.begin(), std::prev(chars.end()));
+	assert(*result == 'x');
 }
 
 // Test case 6: Descending negative elements
 void test3DescNeg() {
-    std::vector<int> nums = {-1, -2, -3};
-    auto result = medianOf3(nums.begin(), std::prev(nums.end()));
-    assert(*result == -2);
+	std::vector<int> nums = {-1, -2, -3};
+	auto result = medianOf3(nums.begin(), std::prev(nums.end()));
+	assert(*result == -2);
 }
 
 int main()
 {
 	std::cout << "Started" << std::endl;
-	
-	test3Distinct();
-	std::cout << "Median of 3 test 1 passed" << std::endl;
-	test3Equal();
-	std::cout << "Median of 3 test 2 passed" << std::endl;
-	test3Ascending();
-	std::cout << "Median of 3 test 3 passed" << std::endl;
-	test3Descending();
-	std::cout << "Median of 3 test 4 passed" << std::endl;
-	test6Chars();
-	std::cout << "Median of 3 test 5 passed" << std::endl;
-	test6Chars();
-	std::cout << "Median of 3 test 5 passed" << std::endl;
-	test3DescNeg();
-	std::cout << "Median of 3 test 6 passed" << std::endl;
 	
 	testEmptyRange();
 	std::cout << "Quicksort test 1 passed" << std::endl;
