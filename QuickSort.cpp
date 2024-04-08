dd5a1b8e28dfdda8277506ee3c8a7c89e4813dc0
Josh <josh@exmaple.com>
2024-04-07 18:20:30 -0700

    Partitions between sizes 4 and 10 (inclusive) are now sorted with insertion sort instead of quicksort. Based on my tests using the time command, this is slightly faster.

--- QuickSort.cpp
+++ QuickSort.cpp
@@ -3,6 +3,7 @@
 #include <vector>
 #include <list>
 #include <cassert>
+#include <algorithm>
 
 // Function prototypes
 template<typename Iter>
@@ -16,6 +17,9 @@ Iter Partition(Iter begin, Iter end);
 
 template <typename Iter>
 Iter medianOf3(Iter begin, Iter end);
+
+template<typename Iter>
+void insertionSort(Iter begin, Iter end);
 
 template <typename Iter>
 void sort2(Iter begin, Iter end);
@@ -38,12 +42,12 @@ void qs(Iter begin, Iter end)
 {
 	while(std::distance(begin, end) > 0)
 	{
-		//std::cout << "distance from begin to end: " << std::distance(begin, end) << std::endl;
-		if (std::distance(begin, end) < 3)
+		if (std::distance(begin, end) < 11)
 		{
 			if (std::distance(begin, end) == 2){ sort3(begin, end);} // More efficient to manually sort 2 or 3 elements than recurse
 			else if (std::distance(begin, end) == 1){ sort2(begin, end);}
-			return; // Partitions of size less than 2 are sorted
+			else if (std::distance(begin, end) < 2) { return; } // Partitions of size less than 2 are sorted
+			else { insertionSort(begin, end); }
 		}
 		Iter pi = Partition(begin, end); // pi is partition index
 		if(std::distance(begin, pi) > std::distance(pi, end)) // recurse smaller partition first
@@ -119,6 +123,17 @@ Iter medianOf3(Iter begin, Iter end)
 	}
 }
 
+// Insertion sort is effective on small ranges
+template<typename Iter>
+void insertionSort(Iter begin, Iter end)
+{
+	assert(std::distance(begin, end) > 2 && std::distance(begin, end) < 11);
+	for(auto i = std::next(begin); i != end; std::advance(i, 1))
+	{
+		std::rotate(std::upper_bound(begin, i, *i), i, std::next(i));
+	}
+}
+
 // Manually sort 2 elements into ascending order
 template <typename Iter>
 void sort2(Iter begin, Iter end)
@@ -153,6 +168,7 @@ void sort3(Iter begin, Iter end)
 	assert(*begin <= *mid && *mid <= *end);
 }
 
+// Functional test cases
 // Quicksort tests
 // Test case 1: empty vector
 void testEmptyRange()
@@ -235,47 +251,6 @@ void testRandomListSort()
 	assert((randomList == std::list<int>{10, 20, 30, 40, 50}));
 }
 
-// Test case 11: large vector with random longs
-void testLongRand()
-{
-	std::vector<long> vec;
-	for(long i = -10000; i <= 50000000; i++)
-	{
-	  if(i % 5 == 0) vec.push_back(i - 20);
-	  else if(i % 3 == 0) vec.push_back(i + 20);
-	  else if(i % 7 == 0) vec.push_back(i + 1);
-	  else vec.push_back(i);
-	}
-	
-	quickSort(vec.begin(), vec.end());
-	
-	for(size_t i = 1; i < vec.size(); i++)
-	{
-	  if(vec.at(i) < vec.at(i-1))
-	  {
-		std::cout << "sort failed at values " << vec.at(i-1) << " and " << vec.at(i) << std::endl;
-		break;
-	  }
-	}
-}
-
-// Test case 12: large vector of duplicates
-void testLongDups()
-{
-	std::vector<long> vec;
-	for(long i = 1; i <= 100000; i++)
-	{
-	  vec.push_back(321);
-	}
-	
-	const auto vec2 = vec;
-	
-	quickSort(vec.begin(), vec.end());
-	
-	assert(vec == vec2);
-}
-
-
 // medianOf3 tests
 // Test case 1: Three distinct elements
 void test3Distinct() {
@@ -319,34 +294,135 @@ void test3DescNeg() {
 	assert(*result == -2);
 }
 
+// Stress Test Cases
+// Test 1: large vector with random longs
+void testLongRand()
+{
+	std::vector<long> vec;
+	vec.reserve(50010000); // Reserve memory for efficiency
+	for(size_t i = -10000; i <= 50000000; i++)
+	{
+	  if(i % 5 == 0) vec.push_back(i - 20);
+	  else if(i % 3 == 0) vec.push_back(i + 20);
+	  else if(i % 7 == 0) vec.push_back(i + 1);
+	  else vec.push_back(i);
+	}
+	
+	quickSort(vec.begin(), vec.end());	
+	assert(std::is_sorted(vec.begin(), vec.end()));
+}
+
+// Test 2: large vector of duplicates
+void testLongDups()
+{
+	std::vector<long> vec;
+	vec.reserve(100000);
+	for(size_t i = 1; i <= vec.capacity(); i++)
+	{
+	  vec.push_back(321);
+	}
+	
+	quickSort(vec.begin(), vec.end());
+	assert(std::is_sorted(vec.begin(), vec.end()));
+}
+
+// Test 3: large vector in ascending order
+void testAscending()
+{
+	std::vector<int> ascendingVec;
+    ascendingVec.reserve(100000000);
+
+    for (size_t i = 1; i <= ascendingVec.capacity(); i++)
+	{
+        ascendingVec.push_back(i);
+    }
+	
+	quickSort(ascendingVec.begin(), ascendingVec.end());
+	assert(std::is_sorted(ascendingVec.begin(), ascendingVec.end()));
+}
+
+// Test 4: large vector in descending order
+void testDescending()
+{
+	std::vector<int> descendingVec;
+    descendingVec.reserve(100000000);
+
+    for (size_t i = descendingVec.capacity(); i <= 1; i--)
+	{
+        descendingVec.push_back(i);
+    }
+	
+	quickSort(descendingVec.begin(), descendingVec.end());
+	assert(std::is_sorted(descendingVec.begin(), descendingVec.end()));
+}
+
+// Test 5: large vector with adjacent elements alternating between positive and negative
+void testAlternating2()
+{
+	std::vector<int> alternatingVec;
+    alternatingVec.reserve(10000000);
+
+    for (size_t i = 1; i <= alternatingVec.capacity(); i++)
+	{
+		int value = (i % 2 == 0) ? -i : i;
+		alternatingVec.push_back(value);
+    }
+	
+	quickSort(alternatingVec.begin(), alternatingVec.end());
+	assert(std::is_sorted(alternatingVec.begin(), alternatingVec.end()));
+}
+
+// Test 6: large vector, mostly ascending except every 10th element is negative
+void testAlternating10()
+{
+	std::vector<int> ascendingVec;
+    ascendingVec.reserve(1000000);
+
+    for (size_t i = 1; i <= ascendingVec.capacity(); i++) {
+        int value = (i % 10 == 0) ? -i : i;
+        ascendingVec.push_back(value);
+    }
+	
+	quickSort(ascendingVec.begin(), ascendingVec.end());
+	assert(std::is_sorted(ascendingVec.begin(), ascendingVec.end()));
+}
+
+
 int main()
 {
 	std::cout << "Started" << std::endl;
-	
 	testEmptyRange();
-	std::cout << "Quicksort test 1 passed" << std::endl;
+	std::cout << "Quicksort functional test 1 passed" << std::endl;
 	testSingleElementRange();
-	std::cout << "Quicksort test 2 passed" << std::endl;
+	std::cout << "Quicksort functional test 2 passed" << std::endl;
 	testSortedRange();
-	std::cout << "Quicksort test 3 passed" << std::endl;
+	std::cout << "Quicksort functional test 3 passed" << std::endl;
 	testReverseSortedRange();
-	std::cout << "Quicksort test 4 passed"	<< std::endl;
+	std::cout << "Quicksort functional test 4 passed"	<< std::endl;
 	testRandomdRangeInt();
-	std::cout << "Quicksort test 5 passed" << std::endl;
+	std::cout << "Quicksort functional test 5 passed" << std::endl;
 	testRandomRangeChar();
-	std::cout << "Quicksort test 6 passed" << std::endl;
+	std::cout << "Quicksort functional test 6 passed" << std::endl;
 	testDuplicateElements();
-	std::cout << "Quicksort test 7 passed" << std::endl;
+	std::cout << "Quicksort functional test 7 passed" << std::endl;
 	void allDupsOdd();
-	std::cout << "Quicksort test 8 passed" << std::endl;
+	std::cout << "Quicksort functional  test 8 passed" << std::endl;
 	void allDupsEven();
-	std::cout << "Quicksort test 9 passed" << std::endl;
+	std::cout << "Quicksort functional test 9 passed" << std::endl;
 	testRandomListSort();
-	std::cout << "Quicksort test 10 passed" << std::endl;
+	std::cout << "Quicksort functional  test 10 passed" << std::endl;
 	testLongRand();
-	std::cout << "Quicksort test 11 passed" << std::endl;
+	std::cout << "Quicksort stress test 1 passed" << std::endl;
 	testLongDups();
-	std::cout << "Quicksort test 12 passed" << std::endl;
+	std::cout << "Quicksort stress test 2 passed" << std::endl;
+	testAscending();
+	std::cout << "Quicksort stress test 3 passed" << std::endl;
+	testDescending();
+	std::cout << "Quicksort stress test 4 passed" << std::endl;
+	testAlternating2();
+	std::cout << "Quicksort stress test 5 passed" << std::endl;
+	testAlternating10();
+	std::cout << "Quicksort stress test 6 passed" << std::endl;
 	std::cout << "Completed" << std::endl;
 	
 	return 0;
